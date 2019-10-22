3e83efb5fe6d59a109d92bf35b0b9f6f91a65add
Magnus Thor <magnus.thor.ar@gmail.com>
2019-10-22 15:08:14 +0200

    Using JPA entities

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -7,13 +7,16 @@ import java.util.Set;
 /**
  * Created by jt on 5/16/17.
  */
-
+@Entity
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String firstName;
     private String lastName;
 
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books  = new HashSet<>();
 
     public Author() {
@@ -30,13 +33,9 @@ public class Author {
         this.books = books;
     }
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public void setId(Long id) { this.id = id; }
 
     public String getFirstName() {
         return firstName;
